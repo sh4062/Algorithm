f1f2d720d93218512a7e7561dc484d4e7d158cf4
sh4062 <sh4062@github.com>
2018-03-02 17:30:26 +0800

    Update

---
+++ PAT/B1002.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<string>
+#include<map>
+using namespace std;
+int main(){
+    string n;
+    int res = 0;
+    map<int, string> r; 
+    string re; 
+    r.insert(map<int, string>::value_type (0, "ling"));  
+    r.insert(map<int, string>::value_type (1, "yi"));  
+  
+    r.insert(map<int, string>::value_type (2, "er"));  
+  
+    r.insert(map<int, string>::value_type (3, "san"));  
+    r.insert(map<int, string>::value_type (4, "si"));  
+    r.insert(map<int, string>::value_type (5, "wu"));  
+    r.insert(map<int, string>::value_type (6, "liu"));  
+    r.insert(map<int, string>::value_type (7, "qi"));  
+    r.insert(map<int, string>::value_type (8, "ba"));  
+    r.insert(map<int, string>::value_type (9, "jiu"));  
+    
+    cin>>n;
+    int i = 0;
+    string j;
+    //cout<<n.size()<<endl;
+    for(i=0;i<n.size();i++){
+       
+        res = res + ((n.at(i)))-48;
+        //cout<<n;
+       
+    }
+    //cout<<res;
+    re = re + r.at(res%10);
+    res = res/10;
+    while(res>0){
+        i = res%10;
+        j = r.at(i);
+        re = j + " " + re;
+        res = res/10; 
+    }
+    cout<<re;
+    return 0;
+}
