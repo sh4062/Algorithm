5acbcdad9d6775d5b421f0ecd50426277177242f
sh4062 <sh4062@github.com>
2018-04-16 14:12:08 +0800

    Update

--- PAT/test.cpp
+++ PAT/test.cpp
@@ -1,83 +1,85 @@
-#include <stdio.h>
+#include <iostream>
+#include <cstdio>
+#include <algorithm>
 #include <queue>
+#include <cstring>
 using namespace std;
-int serve_time[1001];
-int ans[1001];
-queue<int> Q[21];
-int T[21];
-int main(void)
+const int maxn=10000;
+int n,k;
+struct Customer{
+    int arrive; //到达时间
+    int process; //服务时间
+    int start;  //客户开始被服务的时间
+    int finish; //客户的结束时间
+    int wait; //客户的等待时间
+    bool operator<(const Customer tmp)const{
+        return arrive<tmp.arrive;
+    }
+
+}cus[maxn];
+struct Window{
+    int finish_time;
+    bool operator<(const Window tmp)const{
+        return finish_time>tmp.finish_time;
+    }
+};
+int main()
 {
-    int w, cap, cus, k; //分别记录窗口数量、窗口最大人数、顾客数量和查询数量
-    int i, j;
-    while (scanf("%d%d%d%d", &w, &cap, &cus, &k) != EOF)
-    {
-        for (i = 1; i <= cus; i++)
-        {
-            scanf("%d", &serve_time[i]);
+    int h,m,s,t;
+    scanf("%d %d",&n,&k);
+    for(int i=0;i<n;i++){
+        scanf("%d:%d:%d %d",&h,&m,&s,&t);
+        cus[i].arrive=h*3600+m*60+s;
+        if(t>60)
+            t=60; //样例中其实也没有超过60分钟的数据，题目说了是假设
+        cus[i].process=t*60;
+    }
+    priority_queue<Window>q;
+    Window w;
+    for(int i=0;i<k;i++){
+        w.finish_time=8*3600;
+        q.push(w);
+    }
+    sort(cus,cus+n);
+    Window ww;
+    for(int i=0;i<n;i++){
+        w=q.top();
+        q.pop();
+        if(cus[i].arrive<w.finish_time){
+            cus[i].wait=w.finish_time-cus[i].arrive;
+            cus[i].start=w.finish_time;
+            cus[i].finish=cus[i].start+cus[i].process;
+            ww.finish_time=cus[i].finish;
+            q.push(ww);
         }
-        for (i = 0; i < w; i++)
-        {
-            if (Q[i].empty() == false)
-                Q[i].pop();
-        }
-
-        int sum = 0;
-        int count = 1;
-        for (int ti = 0; ti < 540; ti = ti + 1)
-        { //以时间来作为循环，这个很关键
-            for (i = 0; i < w; i++)
-            {
-                for (j = 0; j < Q[i].size(); j++)
-                {
-                    if (ti == ans[Q[i].front()])
-                    { //如果当前队伍的人服务结束了
-                        Q[i].pop();
-                        sum--;
-
-                        if (!Q[i].empty())
-                        { //并且计算当前队伍下一个人的结束时间
-                            int tmp = Q[i].front();
-                            ans[tmp] = ti + serve_time[tmp]; //结束时间为当前时间+顾客的服务时间
-                        }
-                    }
-                }
-            }
-
-            while (sum < w * cap && count <= cus)
-            {
-                int min = 0;
-                for (i = 0; i < w; i++)
-                {
-                    if (Q[min].size() > Q[i].size())
-                    {
-                        min = i;
-                    } //找到人最少的那个队伍
-                    if (Q[min].size() == 0)
-                        ans[count] = ti + serve_time[count]; //如果队伍没人则直接开始服务，并且计算结束时间
-                    if (Q[min].size() < cap && count <= cus)
-                    {
-                        Q[min].push(count); //否则把让下一个顾客进队
-                        count++;
-                        sum++;
-                    }
-                }
-            }
-        }
-
-        for (i = 0; i < k; i++)
-        {
-            int query;
-            scanf("%d", &query);
-            if (ans[query] == 0)
-                puts("Sorry"); //值为0说明没有被开始服务，只能Sorry
-            else
-            {
-                int hour, min;
-                hour = 8 + ans[query] / 60;
-                min = ans[query] % 60;
-                printf("%02d:%02d\n", hour, min); //把时间转换为标准格式并输出
-            }
+        else{
+            cus[i].start=cus[i].arrive;
+            cus[i].wait=0;
+            cus[i].finish=cus[i].start+cus[i].process;
+            ww.finish_time=cus[i].finish;
+            q.push(ww);
         }
     }
+    double sum=0;
+    int cnt=0;
+    for(int i=0;i<n;i++){
+        /*
+        一开始和P1014搞混了，以为如果是17点之前还没被服务，就不考虑
+        后来发现题目中明确说了，是17点之后到的才不考虑
+        */
+        if(cus[i].arrive<=17*3600){
+            sum+=cus[i].wait;
+            cnt++;
+        }
+    }
+    if(cnt==0)
+        printf("0.0\n"); //去掉这个也能AC，说明样例没有出现cnt为0的情况
+    else{
+        double minutes=sum/60.0/cnt;
+        printf("%.1lf\n",minutes);
+    }
+    printf("%d\n",cnt);
+
+
     return 0;
 }
